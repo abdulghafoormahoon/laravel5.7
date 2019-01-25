5774a12f7a8ff60bcdd4cbe59a091963d1ec39c7
Abdul Ghafoor <>
2019-01-25 12:22:07 +0000

    Create env file for travis

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/', function () {
+        return view('welcome');
+    }
+);
 
-Route::resource('products','ProductController');
+Route::resource('products', 'ProductController');
